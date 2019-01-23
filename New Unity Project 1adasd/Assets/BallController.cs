7f2c861736e10601c4b23a32febbc24312ab1893
Nisha Shlaymoon <nishatoma@gmail.com>
2019-01-22 22:31:31 -0500

    Added Shapes and controllers
    Added Shapes and controllers

---
+++ New Unity Project 1adasd/Assets/BallController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed = 10.0f;
+	public float rotationSpeed = 100.0f;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update() {
+		//transform.Translate (Vector3.forward * 10 * Time.deltaTime);
+	}
+
+	void FixedUpdate () {
+		/*
+		 * 
+		float moveHorizontal = Input.GetAxis ("Horizontal") * speed;
+		float moveVertical = Input.GetAxis ("Vertical") * rotationSpeed;
+
+		Vector3 movement = new Vector3 (-moveVertical, 0, moveHorizontal);
+
+		// If the player presses space
+		if (Input.GetKey(KeyCode.Space)) {
+			Vector3 up = new Vector3 (0, 10, 0);
+			rb.AddForce (up * speed/5);
+		}
+
+		rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+		rb.AddForce (movement);
+		*/
+	}
+}
---
+++ New Unity Project 1adasd/Assets/CameraController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Get an instance of the sphere
+	public GameObject player;
+
+	// Offset for the camera
+	private Vector3 offset;
+	public float panSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		//offset = transform.position - player.transform.position;\
+		
+	}
+	
+	// Update is used for following cameras
+	// Just like update, runs every frame, but it is guaranteed
+	// to run after all items have been proccessed in update.
+	void LateUpdate () {
+		Vector3 pos = transform.position;
+		Vector3 rot = transform.rotation;
+
+		// Every frame
+		//transform.position = player.transform.position + offset;
+		if (returnKey ().Equals ("t")) {
+			if (Input.GetKey (KeyCode.W)) {
+				pos.z += panSpeed * Time.deltaTime;
+			} else if (Input.GetKey (KeyCode.S)) {
+				pos.z -= panSpeed * Time.deltaTime;
+			} else if (Input.GetKey (KeyCode.A)) {
+				pos.x -= panSpeed * Time.deltaTime;
+			} else if (Input.GetKey (KeyCode.D)) {
+				pos.x += panSpeed * Time.deltaTime;
+			}
+			transform.position = pos;
+		} else if (returnKey ().Equals ("r")) {
+			if (Input.GetKey (KeyCode.W)) {
+				rot.z += panSpeed * Time.deltaTime;
+			} else if (Input.GetKey (KeyCode.S)) {
+				rot.z -= panSpeed * Time.deltaTime;
+			} else if (Input.GetKey (KeyCode.A)) {
+				rot.x -= panSpeed * Time.deltaTime;
+			} else if (Input.GetKey (KeyCode.D)) {
+				rot.x += panSpeed * Time.deltaTime;
+			}
+			transform.rotation = rot;
+		}
+	}
+
+	string returnKey() {
+		if (Input.GetKey (KeyCode.T)) {
+			// Translation
+			return "t";	
+		} else if (Input.GetKey (KeyCode.R)) {
+			// Rotation
+			return "r";
+		} else {
+			return "";
+		}
+	}
+}
